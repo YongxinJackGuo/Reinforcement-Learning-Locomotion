479862b02921b3881ac0ad5992525f2319b47b58
zhuoyuhe <60048976+zhuoyuhe@users.noreply.github.com>
2020-05-03 18:12:41 -0400

    Add files via upload
    version 5.3

--- utils/common.py
+++ utils/common.py
@@ -10,28 +10,31 @@ def cg(b, kl, pi, cg_iter, threshold):
     x = torch.zeros(b.shape)
     # r is non-conjugate component, also the residual, d is conjugate component
     r = d = b.clone()  # Originally, it is r = d = b - Hx but Hx is zero anyway as the initialization
+    kl = kl.mean()
+    grad = torch.autograd.grad(kl, pi.parameters(), create_graph=True)
     for _ in range(cg_iter):
-        beta = -1 * torch.dot(d, r) / torch.dot(d, compute_hvp(kl, d, pi))  # stepsize along for solution
+        hvp = compute_hvp(kl, d, pi, grad)
+        beta = -1 * torch.dot(d, r) / torch.dot(d, hvp)  # stepsize along for solution
         x += beta * d  # update solution along conjugate direction
-        r_prev = r # store old r
-        r -= beta * compute_hvp(kl, d, pi)  # update non-conjugate direction
-        delta = torch.dot(r, r) / torch.dot(r_prev, r_prev)  # update stepsize for conjugate direction
-        d = r - torch.dot(delta, d)
-        if r < threshold:
+        r_prev = r.clone() # store old r
+        r -= beta * hvp  # update non-conjugate direction
+        r_curr = torch.dot(r, r)
+        delta = r_curr / torch.dot(r_prev, r_prev)  # update stepsize for conjugate direction
+        d = r - delta * d
+
+        if r_curr < threshold:
             break
     return x
 
 # Perform Hessian-vector product
-def compute_hvp(kl, v, pi):
+def compute_hvp(kl, v, pi, grad):
     # TODO: compute Hessian-vector product from KL
     #  In this case, the Hessian matrix is Fisher Information Matrix (FIM)
-    kl = kl.mean()
-    grad = torch.autograd.grad(kl, pi.parameters(), create_graph=True)  # Compute d(KL)/dx
+    # grad = torch.autograd.grad(kl, pi.parameters(), create_graph=True)  # Compute d(KL)/dx
     flat_grad = torch.cat([g.view(-1) for g in grad])  # Flatten
     grad_v = torch.dot(flat_grad, v)  # Compute d(KL)/dx * v
-    grad_grad_v = torch.autograd.grad(grad_v, pi.parameters())  # Compute d/dx * d(KL)/dx * v. Hessian-vector product
+    grad_grad_v = torch.autograd.grad(grad_v, pi.parameters(), retain_graph=True)  # Compute d/dx * d(KL)/dx * v. Hessian-vector product
     flat_grad_grad_v = torch.cat([gg.view(-1) for gg in grad_grad_v])  # Flatten
-
     return flat_grad_grad_v  # Can add a damping term to it
 
 # Compute KL divergence between two different policy nets
@@ -45,17 +48,19 @@ def compute_KL(new_stats, old_stats):
     #  distribution similarity for line search.
     #  KL(p, q) = log(std_q / std_p) + (std_p + (mean_p - mean_q)^2) / 2std_q^2 - 0.5
     mu_new, std_new = new_stats[0], new_stats[1]
+    # print(mu_new)
+    # print('-----------------------------')
     mu_old, std_old = old_stats[0], old_stats[1]
+    # print(mu_old)
     # Compute KL divergence of next policy distributin w.r.t current policy distribution
     kl = torch.log(std_old / std_new) + (std_new + (mu_new - mu_old).pow(2)) / (2 * std_old.pow(2)) - 0.5
-
+    # print(kl)
     return kl.sum(dim=1).mean()
 
 # Get General Advantage Estimate (GAE)
-def get_adv(obs, rwd, value_model, gamma, Lambda, ep_len):
+def get_adv(rwd, gamma, Lambda, ep_len, value):
     # TODO: get estimate advantage function GAE
     # Remember to normalize the advantages for training stability. (x - mu) / std
-    value = value_model(obs)
     delta = torch.zeros_like(value)
     adv = torch.zeros_like(value)
     pass_time = 0
@@ -88,13 +93,16 @@ def line_search(stepsize, pi, obs, acs, args):
         new_params = cur_params + new_stepsize
         set_flat_param(pi, new_params)  # update the model
         new_stats = (pi(obs))  # store new model stats
+        # print(new_stats)
         new_log_prob = pi.get_log_prob(obs, acs)  # new policy log probability for actions
 
         # compute criteria
         L = (new_log_prob - old_log_prob).mean()
-        kl = compute_KL(old_stats, new_stats)
+        kl = compute_KL(new_stats, old_stats)
+        print(L)
+        print(kl)
         if kl < 1.5 * max_KL and L > 0:  # Trust Region Condition
-            print('Step {:.2f} size accepted'.format(new_stepsize))
+            print('Step {:.6f} size accepted'.format(new_stepsize.detach().numpy()[0]))
             return True, new_params  # Step size accept
 
     return False, new_params
@@ -111,7 +119,7 @@ def set_flat_param(model, flat_param):
     for param in model.parameters():
         num_elem = param.numel()
         param.data.copy_(flat_param[count: count + num_elem].reshape_as(param))
-        count = num_elem
+        count += num_elem
 
 def get_flat_grad(model):
     # TODO: Get the network parameter gradients
