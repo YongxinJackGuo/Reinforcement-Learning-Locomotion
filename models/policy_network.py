1ce29960f8d06ac25a12c8dcb50324dcdd2033d9
zhuoyuhe <60048976+zhuoyuhe@users.noreply.github.com>
2020-05-03 18:13:39 -0400

    Add files via upload
    version 5.3

--- models/policy_network.py
+++ models/policy_network.py
@@ -13,22 +13,25 @@ class Policy_Net(torch.nn.Module):
         self.fc2 = torch.nn.Linear(100, 50)
         self.fc3 = torch.nn.Linear(50, 25)
         self.fc4 = torch.nn.Linear(25, ac_dim)
-        self.tanh1 = torch.nn.Tanh()
-        self.tanh2 = torch.nn.Tanh()
-        self.tanh3 = torch.nn.Tanh()
-        self.tanh4 = torch.nn.Tanh()
-
+        # self.tanh1 = torch.nn.Tanh()
+        # self.tanh2 = torch.nn.Tanh()
+        # self.tanh3 = torch.nn.Tanh()
+        # self.tanh4 = torch.nn.Tanh()
 
     def forward(self, obs):
+        obs = torch.Tensor(obs)
         x = self.fc1(obs)
-        x = self.tanh1(x)
+        x = torch.tanh(x)
+        # x = self.tanh1(x)
         x = self.fc2(x)
-        x = self.tanh2(x)
+        x = torch.tanh(x)
+        # x = self.tanh2(x)
         x = self.fc3(x)
-        x = self.tanh3(x)
+        x = torch.tanh(x)
+        # x = self.tanh3(x)
         x = self.fc4(x)
         mu = x
-        log_std = torch.zeros_like(mu)
+        log_std = torch.zeros_like(mu)-1
         std = torch.exp(log_std)
         return mu, std
 
@@ -38,6 +41,7 @@ class Policy_Net(torch.nn.Module):
         #  KL(p, q) = log(std_q / std_p) + (std_p + (mean_p - mean_q)^2) / 2std_q^2 - 0.5
         # Compute mu, std of next policy as variable. (Requires grad)
         mu_next, std_next = self.forward(obs)
+
         # Compute mu, std of current policy as fixed constant. (Detached from autograd)
         mu_cur, std_cur = mu_next.detach(), std_next.detach()
         # Compute KL divergence of next policy distributin w.r.t current policy distribution
@@ -46,10 +50,11 @@ class Policy_Net(torch.nn.Module):
         return KL.sum(dim=1)  # sum up over each batch. The return dim will be N.(batch size)
 
     def get_log_prob(self, obs, acs):
+        acs = torch.from_numpy(acs)
         PI = torch.tensor([math.pi])
         mu, std = self.forward(obs)
         # Compute log probability of actions within a batch
-        log_prob = -torch.log(mu * torch.sqrt(2 * PI)) - 0.5 * ((acs - mu) / std).pow(2)
+        log_prob = -torch.log(std * torch.sqrt(2 * PI)) - 0.5 * ((acs - mu) / std).pow(2)
         return log_prob
 
     def sample_action(self, obs):
--- models/value_network.py
+++ models/value_network.py
@@ -1,4 +1,8 @@
 import torch
+from models import policy_network
+from agents import ant
+import numpy as np
+from torch.utils.data import TensorDataset, DataLoader
 
 class Value_Net(torch.nn.Module):
     def __init__(self, ob_dim, value_dim):
@@ -10,18 +14,43 @@ class Value_Net(torch.nn.Module):
         self.fc2 = torch.nn.Linear(100, 50)
         self.fc3 = torch.nn.Linear(50, 25)
         self.fc4 = torch.nn.Linear(25, value_dim)
-        self.tanh1 = torch.nn.Tanh()
-        self.tanh2 = torch.nn.Tanh()
-        self.tanh3 = torch.nn.Tanh()
-        self.tanh4 = torch.nn.Tanh()
-
+        # self.tanh1 = torch.nn.Tanh()
+        # self.tanh2 = torch.nn.Tanh()
+        # self.tanh3 = torch.nn.Tanh()
 
     def forward(self, obs):
+        obs = torch.Tensor(obs)
         x = self.fc1(obs)
-        x = self.tanh1(x)
+        # x = self.tanh1(x)
+        x = torch.tanh(x)
         x = self.fc2(x)
-        x = self.tanh2(x)
+        x = torch.tanh(x)
+        # x = self.tanh2(x)
         x = self.fc3(x)
-        x = self.tanh3(x)
+        x = torch.tanh(x)
+        # x = self.tanh3(x)
         x = self.fc4(x)
         return x
+
+    def net_initial(self, agent, policy_net, gamma=0.98, max_iter=100, lr=0.01):
+        # initial the value network for the first time
+        dic = agent.get_traj_per_batch(policy_net)
+        reward = dic['rews']
+        ep_len = dic['ep_len']
+        obs = dic['ob']
+        values = dic['values']
+        reward_tensor = torch.Tensor(reward)
+        dataset = TensorDataset(torch.Tensor(obs), values)
+        dataloader = DataLoader(dataset, batch_size=100, shuffle=True)
+        loss_func = torch.nn.MSELoss()
+
+        optimizer = torch.optim.SGD(self.parameters(), lr=lr, momentum=0.9)
+        for iter in range(max_iter):
+            for batch_idx, (data, target) in enumerate(dataloader):
+                optimizer.zero_grad()
+                predict = self.forward(data)
+                loss = loss_func(predict.flatten(), target)
+                loss.backward()
+                optimizer.step()
+            predict_test = self.forward(obs)
+            loss = loss_func(predict_test.flatten(), values)
