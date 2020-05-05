70195f6063847e16a3167a4b053ff88fb696426c
zhuoyuhe <60048976+zhuoyuhe@users.noreply.github.com>
2020-05-05 12:45:53 -0400

    Add files via upload

--- core/trpo.py
+++ core/trpo.py
@@ -2,6 +2,7 @@ from torch import optim
 import torch.nn as nn
 import torch
 from utils import common as U
+import scipy.optimize
 
 # Trust Region Policy Optimization Update per Batch
 
@@ -9,6 +10,27 @@ def trpo_update(policy_net, value_net, batch_actions, batch_values, batch_states
 
     value_net_lr, l2_reg, max_KL = args.value_net_lr, args.l2_reg, args.max_KL
     LBFGS_iters, cg_iters, cg_threshold = args.LBFGS_iters, args.cg_iters, args.cg_threshold
+
+    # def get_value_loss(flat_params):
+    #     U.set_flat_param(value_net, torch.Tensor(flat_params))
+    #     for param in value_net.parameters():
+    #         if param.grad is not None:
+    #             param.grad.data.fill_(0)
+    #     values_pred = value_net(batch_states)
+    #     value_loss = (values_pred - batch_values).pow(2).mean()
+    #
+    #     # weight decay
+    #     for param in value_net.parameters():
+    #         value_loss += param.pow(2).sum() * l2_reg
+    #     value_loss.backward()
+    #     return value_loss.item(), U.get_flat_param(value_net.parameters()).numpy()
+    #
+    # flat_params, _, opt_info = scipy.optimize.fmin_l_bfgs_b(get_value_loss,
+    #                                                         U.get_flat_param(value_net).detach().cpu().numpy(),
+    #                                                         maxiter=25)
+    # U.set_flat_param(value_net, torch.Tensor(flat_params))
+
+
 
     # TODO: compute the policy_net loss function for computing the gradient g later on
     #-------------Compute the policy gradient first---------------------
@@ -18,7 +40,6 @@ def trpo_update(policy_net, value_net, batch_actions, batch_values, batch_states
     log_prob_cur = policy_net.get_log_prob(batch_states, batch_actions).detach()
 
     # Construct a policy loss Function: L = (pi_prob_next / pi_prob_cur) * Advantages
-    test = (torch.exp(log_prob_next - log_prob_cur))
     L_policy = (torch.exp(log_prob_next - log_prob_cur)).t() * adv# negative sign for minimization convention
     L_policy = L_policy.sum()/adv.shape[0]
 
@@ -34,6 +55,8 @@ def trpo_update(policy_net, value_net, batch_actions, batch_values, batch_states
     KL = policy_net.get_KL(batch_states)
     # Compute x = H^-1 * g using conjugate gradient method
     x = U.cg(pi_grads, KL, policy_net, cg_iters, cg_threshold)  # -pi_grads because we took a minus sign at L before
+    if x is False:
+        return False, value_net, policy_net
     # Compute Hx
     KL = KL.mean()
     grad = torch.autograd.grad(KL, policy_net.parameters(), create_graph=True)
