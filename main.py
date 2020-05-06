e87eda7a2840bad8729b3832a9cb89139a0f51ea
zhuoyuhe <60048976+zhuoyuhe@users.noreply.github.com>
2020-05-06 09:03:58 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -12,22 +12,22 @@ from core import trpo
 from matplotlib import pyplot as plt
 import time
 
-seed = 42
-torch.manual_seed(seed)
-np.random.seed(42)
+# seed = 42
+# torch.manual_seed(seed)
+# np.random.seed(42)
 
 
 class Args():
     def __init__(self):
-        self.max_iters = 500
-        self.horizon = 5000 # Batch size: time steps per batch
-        self.episode_long = 5000
+        self.max_iters = 1000
+        self.horizon = 100000 # Batch size: time steps per batch
+        self.episode_long = 1000
         self.l2_reg = 1E-4  # L2 regularization lambda for value loss function
         self.max_KL = 0.01 # Max KL divergence threshold for TRPO update
         # Environemnt
-        self.env = HopperEnv() #ant_v3.AntEnv(ctrl_cost_weight=1E-6, contact_cost_weight=1E-3, healthy_reward=0.05)
-        self.env.seed(seed)
-        self.agent = Ant(self.env, self.horizon)  # create agent
+        self.env = Walker2dEnv() #ant_v3.AntEnv(ctrl_cost_weight=1E-6, contact_cost_weight=1E-3, healthy_reward=0.05)
+        # self.env.seed(seed)
+        self.agent = Ant(self.env, self.horizon,self.episode_long)  # create agent
         self.pi_net = Policy_Net(self.agent.ob_dim, self.agent.ac_dim)  # Create Policy Network
         self.value_net = Value_Net(self.agent.ob_dim, 1)  # Create Value Network
         self.value_net_lr = 1  # Declare value net learning rate
@@ -66,10 +66,11 @@ def train(args):
         # assert success is True, "Linear Search False"
         print('iter %d' % (iters))
         print(success)
+        if iters % 100 == 0:
+            np.savetxt('hopper.csv', [np.array(value_list)], delimiter=',', fmt='%f')
+            np.savetxt('hopper_len.csv', [np.array(len_list)], delimiter=',', fmt='%f')
     args.value_net = value_net
     args.pi_net = pi_net
-    np.savetxt('hopper.csv', [np.array(value_list)], delimiter=',', fmt='%f')
-    np.savetxt('hopper_len.csv', [np.array(len_list)], delimiter=',', fmt='%f')
     return args
 
 def show(args, iter=10000):
